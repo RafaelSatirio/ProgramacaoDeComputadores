19a6bed23595367060918214cd3475776c40ef8b
RafaelSatirio <125366637+RafaelSatirio@users.noreply.github.com>
2024-04-16 10:36:14 -0300

    exercicio 4.3 feito

---
+++ capitulo4/ExercicioMedia10Alunos/notaAcimaVetor.py
@@ -0,0 +1,14 @@
+﻿vetClasse = [0] * (11)
+
+soma = 0
+notaAcima = 0
+for x in range(1, 10 + 1, 1):
+    print("digite a nota do aluno: " + str(x))
+    vetClasse[x] = float(input())
+for x in range(1, 10 + 1, 1):
+    soma = soma + vetClasse[x]
+media = soma / 10
+for x in range(1, 10 + 1, 1):
+    if vetClasse[x] > media:
+        notaAcima = notaAcima + 1
+print("Números de alunos com a nota acima da média = " + str(notaAcima))
